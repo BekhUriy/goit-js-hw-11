7b8638628b9c2f6273684d8b449c25e4fcc71150
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2023-11-07 14:03:05 +0200

    fix_10

--- src/index.js
+++ src/index.js
@@ -1,101 +1,167 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const apiKey = '40367315-3c91d510b26c4724b33f253c9'; // Підстав свій унікальний ключ доступу
-
-axios.defaults.params = {
-  key: apiKey,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-  per_page: 40, // Кількість зображень на сторінці
+const element = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  input: document.getElementById('search-query'),
+  loadMore: document.querySelector('.load-more'),
 };
 
-const searchForm = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
-loadMoreBtn.classList.add('is-hidden');
+let query = '';
+let page = 0;
+let totalHits = 0;
+let changedQuery = false;
+let simplelightbox;
 
-let page = 1; // Початкова сторінка для пагінації
-gallery.innerHTML = '';
+element.loadMore.style.display = 'none';
 
-const fetchImages = async searchQuery => {
-  try {
-    const response = await axios.get('https://pixabay.com/api/', {
-      params: {
-        q: searchQuery,
-        page: page,
-      },
-    });
-    return response.data.hits;
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    return [];
-  }
-};
+const perPage = 40;
 
-const displayImages = images => {
-  const lightbox = new SimpleLightbox('.gallery a', {});
+element.loadMore.addEventListener('click', handlerLoadMore);
+element.form.addEventListener('submit', handlerSearch);
 
-  images.forEach(image => {
-    const card = document.createElement('div');
-    card.classList.add('photo-card');
+async function handlerSearch(evt) {
+  evt.preventDefault();
 
-    card.innerHTML = `
-      <a href="${image.largeImageURL}" data-lightbox="gallery-item">
-        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-      </a>
-      <div class="info">
-        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-        <p class="info-item"><b>Views:</b> ${image.views}</p>
-        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
-      </div>
-    `;
+  const inputValue = element.input.value.trim();
 
-    gallery.appendChild(card);
-  });
-
-  lightbox.refresh(); // Оновлюємо галерею після додавання нових зображень
-  loadMoreBtn.style.display = 'block'; // Показуємо кнопку "Load more" після завантаження зображень
-};
-
-searchForm.addEventListener('submit', async e => {
-  e.preventDefault();
-  const searchQuery = document.querySelector("input[name='searchQuery']").value;
-
-  gallery.innerHTML = ''; // Очищаємо галерею перед новим пошуком
-  page = 1;
-
-  const images = await fetchImages(searchQuery);
-
-  if (images.length > 0) {
-    displayImages(images);
-    page = 2; // Переходимо на наступну сторінку для пагінації
-    loadMoreBtn.style.display = 'block'; // Показуємо кнопку "Load more"
-  } else {
-    alert(
-      'Sorry, there are no images matching your search query. Please try again.'
+  if (!inputValue) {
+    return Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.'
     );
-    loadMoreBtn.classList.add('is-hidden');
   }
-});
 
-loadMoreBtn.addEventListener('click', async () => {
-  const searchQuery = document.querySelector("input[name='searchQuery']").value;
-  const images = await fetchImages(searchQuery);
+  if (query !== inputValue) {
+    changedQuery = true;
+    page = 0;
+    totalHits = 0;
+    query = inputValue;
+  }
 
-  if (images.length > 0) {
-    displayImages(images);
-    page++; // Переходимо на наступну сторінку для пагінації
-  } else {
-    loadMoreBtn.classList.add('is-hidden');
-    alert("We're sorry, but you've reached the end of search results.");
-    selectElement.addEventListener('change', event => {
-      const selectedBreedId = event.target.value;
-      showCatInfo(selectedBreedId);
-      page = 1; // Скидаємо номер сторінки при зміні пошукового запиту
+  element.loadMore.style.display = 'none';
+  page++;
+
+  try {
+    const response = await fetchImages(page);
+
+    if (!response || response.data.hits.length === 0) {
+      element.gallery.innerHTML = '';
+      throw new Error('No images found');
+    }
+
+    if (changedQuery) {
+      element.gallery.innerHTML = creatMarkup(response.data.hits);
+    } else {
+      element.gallery.insertAdjacentHTML(
+        'beforeend',
+        creatMarkup(response.data.hits)
+      );
+    }
+
+    simplelightbox = new SimpleLightbox('.gallery a').refresh();
+    Notiflix.Notify.success(
+      `Hooray! We found ${response.data.totalHits} images.`
+    );
+
+    const totalPages = Math.ceil(response.data.totalHits / perPage);
+    if (page > totalPages) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+    element.loadMore.style.display = 'block';
+
+    const cardHeight = element.gallery.firstElementChild.clientHeight;
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
     });
+  } catch (error) {
+    Notiflix.Notify.failure(`Error: ${error.message}. Please try again.`);
   }
-});
+}
+
+async function fetchImages(page) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '40498232-0e7dac35b6ba9fe0d736e222d';
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page,
+    per_page: perPage,
+  };
+
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response;
+  } catch (error) {
+    throw new Error(`Error fetching data: ${error.message}`);
+  }
+}
+
+function creatMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `
+      <a class="gallery-link" href="${largeImageURL}">
+        <div class="photo-card">
+          <div class="gallery-item">
+            <img class="gallery-item-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b> ${likes}</p>
+              <p class="info-item"><b>Views</b> ${views}</p>
+              <p class="info-item"><b>Comments</b> ${comments}</p>
+              <p class="info-item"><b>Downloads</b> ${downloads}</p>
+            </div>
+          </div>
+        </div>
+      </a>
+    `
+    )
+    .join('');
+}
+
+async function handlerLoadMore() {
+  page++;
+  changedQuery = false;
+
+  try {
+    const response = await fetchImages(page);
+    insertCreatMarkup(response);
+
+    simplelightbox = new SimpleLightbox('.gallery a').refresh();
+    Notiflix.Notify.success(
+      `Hooray! We found ${response.data.totalHits} images.`
+    );
+
+    const totalPages = Math.ceil(response.data.totalHits / perPage);
+    if (page > totalPages) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      element.loadMore.style.display = 'none';
+    }
+  } catch (error) {
+    Notiflix.Notify.failure(`Error: ${error.message}. Please try again.`);
+  }
+}
+
+function insertCreatMarkup(response) {
+  const markup = creatMarkup(response.data.hits);
+  element.gallery.insertAdjacentHTML('beforeend', markup);
+}
